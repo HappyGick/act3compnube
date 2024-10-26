871b1ddd1dba03008225b466f75e27a628d2aea6
HappyGick <egarciayustiz@gmail.com>
2024-10-26 16:38:06 -0400

    make directory name unique

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlmodel import Field, SQLModel, Relationship
 
 class Directory(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
+    name: str = Field(index=True, unique=True)
     emails:list['Email'] = Relationship(back_populates='directory', cascade_delete=True)
 
 class Email(SQLModel, table=True):
